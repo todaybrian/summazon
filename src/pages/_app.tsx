81a0357d443c3b95b8ce6350f1e5d326b0a9f17d
todaybrian <todaybryan@gmail.com>
2023-07-08 22:15:35 -0400

    Co-authored-by: Pancreas <Pancraes@users.noreply.github.com> Co-authored-by: Enoch He <EnochHe1@users.noreply.github.com> Co-authored-by: Lucy Qi <lucyqqi@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Urbanist } from 'next/font/google'
+
+const urbanist = Urbanist({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<main className={`${urbanist.className}`}>
+
+    <Component {...pageProps} />
+  </main>);
 }
